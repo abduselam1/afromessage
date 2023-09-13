17ad1198b37394f5d264a1e6f9f7152ffbd4b9e7
abduselam1 <abduselamhafiz@gmail.com>
2023-09-13 09:18:29 +0300

    initail commit

---
+++ src/AfroMessageServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Abduselam\Afromessage;
+
+use Illuminate\Support\ServiceProvider;
+
+class AfroMessageServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        
+    }
+
+}
